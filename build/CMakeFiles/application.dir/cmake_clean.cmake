7d3bce62f427ed0075a2ffb8f3736a7f03e82f76
Steven <steventubusiness@gmail.com>
2024-01-28 01:21:00 -0600

    First Commit

---
+++ build/CMakeFiles/application.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/application_autogen.dir/ParseCache.txt"
+  "application_autogen"
+  "CMakeFiles/application.dir/application_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/application.dir/application_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/application.dir/ui-desktop/src/main.cpp.obj"
+  "CMakeFiles/application.dir/ui-desktop/src/main.cpp.obj.d"
+  "CMakeFiles/application.dir/ui-desktop/src/mainwindow.cpp.obj"
+  "CMakeFiles/application.dir/ui-desktop/src/mainwindow.cpp.obj.d"
+  "application.exe"
+  "application.exe.manifest"
+  "application.pdb"
+  "application_autogen/mocs_compilation.cpp"
+  "application_autogen/timestamp"
+  "libapplication.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/application_autogen_timestamp_deps.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for application_autogen_timestamp_deps.
+# This may be replaced when dependencies are built.
